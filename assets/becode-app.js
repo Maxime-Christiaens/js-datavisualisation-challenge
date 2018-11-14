e97bff85ebd0c20c8a08b84464f9c0f7e841a7fc
Maxime Christiaens <maxime.ad.christiaens@gmail.com>
2018-11-14 12:28:41 +0100

    test

--- assets/becode-app.js
+++ assets/becode-app.js
@@ -14,13 +14,13 @@ What does this script do ?
 
 //Node.insertBefore() : insérer avant un noeud
 
-table1 = document.getElementById("table1");
+const table1 = document.getElementById("table1");
 //id du tableau 1
-table2 = document.getElementById("table2");
+const table2 = document.getElementById("table2");
 //id du tableau 2
-
-const h3Node = document.getElementById("Crimes_et_d.C3.A9lits_enregistr.C3.A9s_par_les_services_de_police");
+const SpawnNode = document.getElementById("Crimes_et_d.C3.A9lits_enregistr.C3.A9s_par_les_services_de_police");
 //récupère la nod de la table1
+const SpawnNode2 = document.getElementById("Homicides");
 
 let h2JS = document.createElement("h2");
 //créer un h2 dans le document
@@ -31,9 +31,9 @@ h2JS.setAttribute("class", "red");
 
 //var insertH2JS = table1Node.insertBefore(h2JS, null);
 
-h3Node.appendChild(h2JS);
+SpawnNode.appendChild(h2JS);
 //insérer le h2 après le h3 
-
+SpawnNode2.appendChild(h2JS);
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
