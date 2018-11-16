22b7a93203ec0858170ee99d816b684d2ed86abc
Maxime Christiaens <maxime.ad.christiaens@gmail.com>
2018-11-16 11:48:38 +0100

    création du tableau contenant les éléments almost done

--- assets/becode-app.js
+++ assets/becode-app.js
@@ -2,7 +2,7 @@
 // WRITE YOUR JAVASCRIPT BELOW THIS COMMENT 
 
 Your name : Maxime Christiaens    
-Date : 14-11-18
+Date : 14-11-18 - 16-11-18
 Contact information : maxime.ad.christiaens@gmail.com
 
 What does this script do ? 
@@ -22,23 +22,62 @@ const SpawnNode = document.getElementById("Crimes_et_d.C3.A9lits_enregistr.C3.A9
 //récupère la nod de la table1
 const SpawnNode2 = document.getElementById("Homicides");
 
-let h2JS = document.createElement("h2");
-//créer un h2 dans le document
-
-h2JS.innerHTML = "bonsoir";
+let divJS = document.createElement("div");
+//créer un div dans le document
+divJS.id = "#graph1"
+//créer un id attaché au divJS
+divJS.innerHTML = "bonsoir";
 //bonsoir va contenir le message bonsoir
-h2JS.setAttribute("class", "red");
+divJS.setAttribute("class", "red");
 
-//var insertH2JS = table1Node.insertBefore(h2JS, null);
+//var insertH2JS = table1Node.insertBefore(divJS, null);
 
-SpawnNode.appendChild(h2JS);
-//insérer le h2 après le h3 
-SpawnNode2.appendChild(h2JS);
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+SpawnNode.appendChild(divJS);
+//insérer le div après le h3 
 
+//SpawnNode2.appendChild(div2JS);
 
-table1TD = table1.getElementsByTagName("td");
+table1TD = table1.getElementsByTagName("td"); //séléction tout les td
+table1TH = table1.getElementsByTagName("th");
 //récupère toute les valeurs du tableaux conserver dans les élémentes td;
 
-//console.log(table1TD);
+console.log(table1TD[0].innerHTML);
+console.log(table1TD[12].innerHTML);
+console.log(table1TH[15].innerHTML);
 //console.log(table1TD[0].innerHTML);
+
+class object {
+    constructor(year, country, crimes) {
+      this.year = year;
+      this.country =  country;
+      this.crimes = crimes;
+    }
+}
+//permet de créer un objet
+
+let data = [];
+let stockage = null;
+
+/*
+for (i=1; i<12 ; i++){
+    stockage = new object(table1TH[(i+4)].innerHTML, table1TD[0].innerHTML, table1TD[i].innerHTML);
+    console.log(stockage);
+    data[i-1] = stockage;//incrémente toutes les données
+}
+*/
+
+for (j=0; j<((37*12)+1); j++){
+    for (i=1; i<12 ; i++){
+        stockage = new object(table1TH[(i+4)].innerHTML, table1TD[(j*12)].innerHTML, table1TD[i+(12*j)].innerHTML);
+        console.log(stockage);
+        data[i-1] = stockage;//incrémente toutes les données
+    }
+}
+
+console.log(data);
+
+
+
+
+
+
