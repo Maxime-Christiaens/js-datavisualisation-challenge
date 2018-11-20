b21f32ef1a64fdaf06ca678a20a92fe683b27546
Maxime Christiaens <maxime.ad.christiaens@gmail.com>
2018-11-20 11:50:06 +0100

    exo  1 done

--- assets/becode-app.js
+++ assets/becode-app.js
@@ -37,29 +37,28 @@ SpawnNode.appendChild(divJS);
 SpawnNode2.appendChild(divJS2);
 //insérer le div après le h3 
 
+///////////////////////////////////////////////////
+///B1.Récupération des informations du tableau 1///
+///////////////////////////////////////////////////
+
+///1. Récupérer l'info\\\
 
 table1TD = table1.getElementsByTagName("td"); //séléction tout les td
 table1TH = table1.getElementsByTagName("th");
 //récupère toute les valeurs du tableaux conserver dans les élémentes td;
 
-//////////////////////////////////////////////////
-///B.Récupération des informations des tableaux///
-//////////////////////////////////////////////////
-
-/////////////////////
-//1. Récupérer l'info
-
-////////////////////////
-//2. Récupérateur d'info
+///2. Récupérateur d'info\\\
 
 class object {
     constructor(year, country, crimes) {
-      this.year = year;
-      this.country =  country;
-      this.crimes = crimes;
+        this.year = year;
+        this.country = country;
+        this.crimes = crimes;
     }
 }
 //permet de créer un objet qui contiendra l'année, le pays et le nombres de crimes
+
+///3. Boucle récupérant l'info\\\
 
 let data = [];
 //stockera toute les données
@@ -69,45 +68,127 @@ let compte = 0;
 //un compte pour connaitre le nombre d'objet non stocké dans data
 let NbrOfCountry = 35;
 //nombre de pays dans la liste 
-/////////////////////////////////////////////////////////A récupérer automatiquement
+//A récupérer automatiquement
 let TDtable1 = 12;
 //nombre de td (cellule) par ligne (tr) dans la tableau 
-////////////////////////////////////////////////////////A récupérer automatiquement
+//A récupérer automatiquement
 
-for (j=0; j<NbrOfCountry; j++){
-    for (i=1; i<TDtable1 ; i++){
-        stockage = new object(table1TH[(i+4)].innerHTML, table1TD[(j*TDtable1)].innerHTML, table1TD[i+(TDtable1*j)].innerHTML);
-        if (stockage == undefined){
-            compte += 1;
+for (j = 0; j < NbrOfCountry; j++) {//la deuxième bouche parcours chaque ligne permettant d'obtenir l'info pour chaque pays
+    for (i = 1; i < TDtable1; i++) {//la première boucle parcours une ligne du talbeau pour en récupérer l'info sur les 10 ans de crimes
+        stockage = new object(table1TH[(i + 4)].innerHTML, table1TD[(j * TDtable1)].innerHTML, table1TD[i + (TDtable1 * j)].innerHTML);
+        if (stockage == undefined) {
+            compte += 1;//si pas de valeur rien n'est incrémenter
         }
-        else if (stockage.crimes == ":"){
-            stockage.crimes = 0;
-            data[((j)*TDtable1)+i] = stockage;//incrémente toutes les données
+        else if (stockage.crimes == ":") {
+            stockage.crimes = 0;//transforma la valeur : en 0 pour que dimpleJS puisse créer le graph sinon bug de dimpleJS
+            data[((j) * TDtable1) + i] = stockage;//incrémente toutes les données
         }
-        else{
-            data[((j)*TDtable1)+i] = stockage;//incrémente toutes les données
-            console.log(stockage);
+        else {
+            data[((j) * TDtable1) + i] = stockage;//incrémente toutes les données
         }
     }
 }
+
 data.shift();
-for (k=1; k<data.length ; k++){
-    if (data[k] == undefined || data[k] == null){
+//retire une donnée vide présente en position zéro du tableau
+for (k = 1; k < data.length; k++) {
+    if (data[k] == null) {
         data.splice(k, 1);
     }
 
-}//retire les valeurs null et non défini
+}
+//retire les valeurs null
 
-console.log(data);  
+////////////////////////////
+///C1.Création du graph 1///
+////////////////////////////
+
 
 let svg = dimple.newSvg("#graph1", 1200, 600);//creating a SVG with given size
 let chart = new dimple.chart(svg, data); // getting a Chart instance to draw on
 chart.addCategoryAxis("x", "year"); // setting the x axis to show the sports
 chart.addMeasureAxis("y", "crimes"); // setting the y axis to show the no. of golds own
 chart.addSeries("country", dimple.plot.line); // plotting, with nationality as series.
+let chartS = chart.addSeries("country", dimple.plot.bubble);//créer des bubbles
 chart.addLegend(750, 60, 200, 600); //ajoute une légende les deux premières valeurs modifie la position en x et y respéctivement
 //quand au deux autres valeurs elle définisse la taille de la légende sur un axe x et y respéctivement
 chart.draw();//dessine la chart
 
 
+///////////////////////////////////////////////////
+///B2.Récupération des informations du tableau 1///
+///////////////////////////////////////////////////
 
+///1. Récupérer l'info\\\
+
+table2TD = table2.getElementsByTagName("td"); //séléction tout les td
+table2TH = table2.getElementsByTagName("th");
+//récupère toute les valeurs du tableaux conserver dans les élémentes td;
+
+///2. Récupérateur d'info\\\
+//déjà créer au plus au au point B1.2.
+
+///3. Boucle récupérant l'info\\\
+
+let data2 = [];
+//stockera toute les données
+let stockage2 = null;
+//contiendra chaque objet
+let compte2 = 0;
+//un compte2 pour connaitre le nombre d'objet non stocké dans data2
+let NbrOfCountry2 = 30;
+//nombre de pays dans la liste 
+//A récupérer automatiquement
+let TDtable2 = 3;
+//nombre de td (cellule) par ligne (tr) dans la tableau 
+//A récupérer automatiquement
+
+
+for (j = 0; j < NbrOfCountry2; j++) {//la deuxième bouche parcours chaque ligne permettant d'obtenir l'info pour chaque pays
+    for (i = 1; i < TDtable2; i++) {//la première boucle parcours une ligne du talbeau pour en récupérer l'info sur les 10 ans de crimes
+        stockage2 = new object(table2TH[(i + 1)].innerHTML, table2TD[(j * TDtable2)].innerHTML, table2TD[i + (TDtable2 * j)].innerHTML);
+        if (stockage2 == undefined) {
+            compte2 += 1;//si pas de valeur rien n'est incrémenter
+        }
+        else if (stockage2.crimes == ":") {
+            stockage2.crimes = 0;//transforma la valeur : en 0 pour que dimpleJS puisse créer le graph sinon bug de dimpleJS
+            data2[((j) * TDtable2) + i] = stockage2;//incrémente toutes les données
+        }
+        else {
+            console.log(stockage2.country.length + stockage2.country);
+            if (stockage2.country.length == 55) {
+                stockage2.country = "Angleterre et Pays de Galles(UK)";
+                data2[((j) * TDtable2) + i] = stockage2;//incrémente toutes les données
+            }
+            data2[((j) * TDtable2) + i] = stockage2;//incrémente toutes les données
+            console.log(stockage2);
+        }
+    }
+}
+
+data2.shift();
+//retire une donnée vide présente en position zéro du tableau
+for (k = 1; k < data2.length; k++) {
+    if (data2[k] == null) {
+        data2.splice(k, 1);
+    }
+
+}
+console.log(data2);
+
+//retire les valeurs null
+
+////////////////////////////
+///C2.Création du graph 1///
+////////////////////////////
+
+
+let svg2 = dimple.newSvg("#graph2", 2400, 500);//creating a SVG with given size
+let chart2 = new dimple.chart(svg2, data2); // getting a Chart instance to draw on
+chart2.addCategoryAxis("x", "year"); // setting the x axis to show the sports
+chart2.addMeasureAxis("y", "crimes"); // setting the y axis to show the no. of golds own
+chart2.addSeries("country", dimple.plot.line); // plotting, with nationality as series.
+let chart2S = chart2.addSeries("country", dimple.plot.bubble);//créer des bubbles
+chart2.addLegend(750, 30, 200, 600); //ajoute une légende les deux premières valeurs modifie la position en x et y respéctivement
+//quand au deux autres valeurs elle définisse la taille de la légende sur un axe x et y respéctivement
+chart2.draw();//dessine la chart2
